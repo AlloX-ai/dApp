d586ed63b1fd95b69e93523443847d5f6f24eac7
LorenaL57 <lorena.lici579@gmail.com>
2026-03-05 15:40:27 +0100

    fixed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -76,7 +76,7 @@ export function Header({
             setShowTooltip(false);
           }}
         >
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-2 sm:gap-4">
             <div
               className="bg-white rounded-full px-3 py-2 flex items-center gap-3 cursor-pointer hover:bg-gray-200 transition-colors"
               onClick={() => {
