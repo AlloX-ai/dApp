44bfab27492117b7379e8ac12a0a578a3db2ecf5
aldialinj <aldialinj0@gmail.com>
2026-03-16 13:56:05 +0100

    added season 1 winners

--- src/constants/addresses.js
+++ src/constants/addresses.js
@@ -1,6 +1,358 @@
 export const season1_winners = [
   {
-    address: "",
-    reward: 30,
+    address: "0x45e0f34e4f98ff321b2226e6943d1c279066486e",
+    reward: 5,
+  },
+  {
+    address: "0xf1b4d55cf1d309e3c1914aa6025d90ffd24910f9",
+    reward: 5,
+  },
+  {
+    address: "0x414113f5e5d0d34d43c589357739016ee27ade11",
+    reward: 5,
+  },
+  {
+    address: "0xe0bba9ef3bd7fb5e875ec21f72ed5e021f68195d",
+    reward: 5,
+  },
+  {
+    address: "0x7502f3e0d21fbd2765bc2a5790f4e51793c6b158",
+    reward: 5,
+  },
+  {
+    address: "0x3b1925168a10632760c4ff9ddac7b6ee7bd476ab",
+    reward: 5,
+  },
+  {
+    address: "0x32b3f9831c8edbb70d1cb542c13ac5da0035a7a7",
+    reward: 100,
+  },
+  {
+    address: "0x2db4625a9ece077cb432d1a84b85b03c15721be6",
+    reward: 5,
+  },
+  {
+    address: "0x53afdcff35029b6ddcc8b742b452e4f99ceca167",
+    reward: 5,
+  },
+  {
+    address: "0x8b6deed7bab8de3ceb438e89476331c4ab07f378",
+    reward: 5,
+  },
+  {
+    address: "0x00a3d4e0134ff2046e9c9b83d3321625ba3da1bd",
+    reward: 5,
+  },
+  {
+    address: "0xa7339edcd5a6e4b814f36dd55a7ba4c1c052ad5a",
+    reward: 5,
+  },
+  {
+    address: "0xfd059b2b44b1b493b3ac5c37c4e852c13ac90191",
+    reward: 5,
+  },
+  {
+    address: "0x76f6988610b1c294d8b0de3e725005af9d557b00",
+    reward: 5,
+  },
+  {
+    address: "0x4f97a3aa19d2a274a96d67b8777f72cc526f3f4c",
+    reward: 5,
+  },
+  {
+    address: "0x8e31d63313b75112f3f33912696a1b264951eadd",
+    reward: 5,
+  },
+  {
+    address: "0x7d919e7cac5acf2e672190250ad208e7734856b3",
+    reward: 5,
+  },
+  {
+    address: "0x1adcd30f688d6cdbd2df141b2e7c4e533364042c",
+    reward: 5,
+  },
+  {
+    address: "0x2768ef0331cfde4cab0ffbf989c8f9d622c64c10",
+    reward: 50,
+  },
+  {
+    address: "0x57cf5e895db619fba06d029b9d1c19af1948eff8",
+    reward: 5,
+  },
+  {
+    address: "0xbd0c31d74b3282e17434d3061be1e488bf8c1ce1",
+    reward: 50,
+  },
+  {
+    address: "0x1c9ececa5a78f6264ae1ad6911dbbdfc8a69aea7",
+    reward: 5,
+  },
+  {
+    address: "0xd0edb048f7d9423d5a5a174dc7746e55c45235ea",
+    reward: 5,
+  },
+  {
+    address: "0x9de651c3918c148ec1e0a4302bb94229c512d3f0",
+    reward: 5,
+  },
+  {
+    address: "0x4450db99260ec3140b4690f1c6bdd64d5d9644a9",
+    reward: 100,
+  },
+  {
+    address: "0x496e2ebb17d81d9bcdb97b75980b952bb9b750b5",
+    reward: 5,
+  },
+  {
+    address: "0x1fa54bc70b7f543e518256b2a666a5f123ad9ad4",
+    reward: 5,
+  },
+  {
+    address: "0xb90001ce1bb77e17ff36ddc22dd902cad4058640",
+    reward: 5,
+  },
+  {
+    address: "0x052a11f02bd2e0d45fc57990b7d4cf06e6851f33",
+    reward: 5,
+  },
+  {
+    address: "0x21d58a9085b39398fa978b069dbaed28aa31e4d5",
+    reward: 5,
+  },
+  {
+    address: "0x4ddbc068d862d45bcd91a1cfc70a6c4dab6e9786",
+    reward: 5,
+  },
+  {
+    address: "0xf5cd336c61b3f2b351d235ecbb22322735930abe",
+    reward: 5,
+  },
+  {
+    address: "0xb0dc3b1f04529984dc65a0a541797b184feb50b9",
+    reward: 5,
+  },
+  {
+    address: "0xf29393374a20af6a64c3211b208dcaf5a966365d",
+    reward: 5,
+  },
+  {
+    address: "0x1b74b46d39ddbfb7d643dca37d0b4bffde4e28ec",
+    reward: 5,
+  },
+  {
+    address: "0x7bec87d012d0c70c8d0327f57a849378cf1daa64",
+    reward: 5,
+  },
+  {
+    address: "0x91e8af43509ea8d804119e845480b3b114d2b369",
+    reward: 5,
+  },
+  {
+    address: "0x56428c73c4fcc0c2b50b281c6f8858fe573bc5b9",
+    reward: 5,
+  },
+  {
+    address: "0xcb4f3802bbb67d6987b9f0acf19eac5e081d29ac",
+    reward: 5,
+  },
+  {
+    address: "0x66f80a55ffd549309a5352202bdd11b96c72d6ed",
+    reward: 5,
+  },
+  {
+    address: "0xc830fe4df0775d1c6ce5541693cbf4210ceac2fb",
+    reward: 5,
+  },
+  {
+    address: "0xf1ba11b03188833d9fa43065c3d0514575a68b8f",
+    reward: 5,
+  },
+  {
+    address: "0xbe0452d64dfcc9fac2782c5ef5dd0ee1eda30041",
+    reward: 5,
+  },
+  {
+    address: "0x8562e4199743671322652dd7955920d7bb0fd372",
+    reward: 5,
+  },
+  {
+    address: "0xfc9d7069bfcaf639d0fe2d47c854a5fcdcf7e5f8",
+    reward: 5,
+  },
+  {
+    address: "0x6e977a02cb1189dff8917d435a19853375b655f0",
+    reward: 5,
+  },
+  {
+    address: "0xde992a0ff78e683aac8d8f044fb532e222f4baa3",
+    reward: 5,
+  },
+  {
+    address: "0xfe1e532e146dd57dc49e36afeaac516b54ede819",
+    reward: 5,
+  },
+  {
+    address: "0x480fdac187f65a80d2559f2bdf7f5db9b8e36d2a",
+    reward: 5,
+  },
+  {
+    address: "0xd7d2194645cb9d841b660f5e00bbcc80d3b52791",
+    reward: 5,
+  },
+  {
+    address: "0xbbab7be8c36d2d818a298fc34a398616329b1d06",
+    reward: 5,
+  },
+  {
+    address: "0x94748e2eb023dde48884b8a1ef3065d6608bb6ef",
+    reward: 5,
+  },
+  {
+    address: "0x97fec670e5de7ace9e4860dd70e1b85459ec5628",
+    reward: 5,
+  },
+  {
+    address: "0x4eacc49acaf72294d8e5fa9fd83b316a08fd320e",
+    reward: 5,
+  },
+  {
+    address: "0xd9932c9ddc4b9ddd08f68c32d97702445ca17c11",
+    reward: 5,
+  },
+  {
+    address: "0x85e95a823ddaf8e8216dbb0a4e2f97a3d31bbf6a",
+    reward: 5,
+  },
+  {
+    address: "0xfe9a5a1684f9cf71ad464617fe4b5898ec8ee1c0",
+    reward: 100,
+  },
+  {
+    address: "0x5acad501c379d3716beaa94bc56f3b72d97b9099",
+    reward: 5,
+  },
+  {
+    address: "0x19be5e68141a4b2a51e37aea6bf3e632cf0d36d7",
+    reward: 5,
+  },
+  {
+    address: "0x3e337bcbaee726a610b9f1bd9bf997b4ac578178",
+    reward: 5,
+  },
+  {
+    address: "0x646715452c3f9f8e31318e6d8808d88f756a07ec",
+    reward: 5,
+  },
+  {
+    address: "0x628fc57a1fc288bb5e8cbc12a0633024554a4e4d",
+    reward: 5,
+  },
+  {
+    address: "0xb5767504310f970712f3c094e15e4035b0be6c1e",
+    reward: 5,
+  },
+  {
+    address: "0x09600a01f7a5d72fd29565793365b98d0e5e8d34",
+    reward: 5,
+  },
+  {
+    address: "0x5d0f8216ab18ae647018e38c99a3b60c83d8a484",
+    reward: 5,
+  },
+  {
+    address: "0x882822cc597fa5b09ceffcd39e82051d0cd521e9",
+    reward: 5,
+  },
+  {
+    address: "0x3637eeca010d66827bbb8bc04f32e609e478bc73",
+    reward: 5,
+  },
+  {
+    address: "0xdabfd2e6cbf04680e2a14d8414586e9f8fc239d2",
+    reward: 5,
+  },
+  {
+    address: "0xcbc127206f3320d7dcdaf158004def6e747a7a9e",
+    reward: 5,
+  },
+  {
+    address: "0xd9c76ca612c43a8332b80bb3026f8abd48d7792b",
+    reward: 5,
+  },
+  {
+    address: "0x7b2d1556d243ee1dd1060c2fa2955926dee208a6",
+    reward: 5,
+  },
+  {
+    address: "0xb6d98de3178da308c88bf6167990cb5218e7455f",
+    reward: 5,
+  },
+  {
+    address: "0xf7af7cfe5524f58e6366d45ecaecdac70d8f24b8",
+    reward: 5,
+  },
+  {
+    address: "DXQB1zkGETDfD82rYwbdQ9JNGMKLnRNbJ74Cww29SAE1",
+    reward: 5,
+  },
+  {
+    address: "0x436b2ffe0b78a151da4c97376f2e70f8fb52c4ff",
+    reward: 5,
+  },
+  {
+    address: "0x00f86548c3cd31259e8f30c2436759de0616d93a",
+    reward: 5,
+  },
+  {
+    address: "0xf45b1f624241cad4d1d27dcb8fcb5c249cba28c3",
+    reward: 5,
+  },
+  {
+    address: "0xafd1a3dee8455a74201a7c78942bf57421ce7e5c",
+    reward: 50,
+  },
+  {
+    address: "0x10947dcf656366373a9e7b3d5fdd278f5290ea4f",
+    reward: 50,
+  },
+  {
+    address: "0xfd3670396678fceefc9e67aa8d90c22a456b77ed",
+    reward: 50,
+  },
+  {
+    address: "0x808a172bae157709e4b90f516808242f33884269",
+    reward: 5,
+  },
+  {
+    address: "0xb73fc5e1c32f71087e280d86d931a41bb055eca3",
+    reward: 5,
+  },
+  {
+    address: "0xbafcc62c95b2350beeebdfd42a3c37c602ab476e",
+    reward: 5,
+  },
+  {
+    address: "0xaaf3630e8bd9f09ac5b9861a734029122c4b194b",
+    reward: 5,
+  },
+  {
+    address: "0xd5e797aad4c3fb4ac4930b064af1321d65a73a8b",
+    reward: 50,
+  },
+  {
+    address: "0x6e8ace82a376d94c1b8c2f88575b35c4c7f6c856",
+    reward: 5,
+  },
+  {
+    address: "0x2e9dd9498f9e500962d07f943fbcdf13738a3906",
+    reward: 5,
+  },
+  {
+    address: "0xe1c4571c0328f71014e0234f7c748755af135a62",
+    reward: 5,
+  },
+  {
+    address: "0x19ebe3c1ee3b3293e8700e54a1d7a1fa324f82b4",
+    reward: 5,
   },
 ];
